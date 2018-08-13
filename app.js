c84e2dd20f78b2ef7e75e62c9f2648a8310182c6
Chris Chu <c20711@gmail.com>
2018-08-13 22:47:30 +1000

    Final commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var expressValidator = require('express-validator');
 var mongojs = require('mongojs');
 var db = mongojs('customerapp', ['users']);
-
+var ObjectId = mongojs.ObjectId;
 var app = express();
 
 // var logger = function(req, res, next) {
@@ -104,6 +104,15 @@ app.post('/users/add', function(req, res) {
       res.redirect('/');
     });
   }
+});
+app.delete('/users/delete/:id', function(req, res) {
+  console.log(req.params.id);
+  db.users.remove({ _id: ObjectId(req.params.id) }, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect('/');
+  });
 });
 app.listen(3000, function() {
   console.log('server started on port 3000...');
